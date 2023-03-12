b9acd749050d4709917891ed86896ea2f134e579
David Carmo <davidsc2812@gmail.com>
2023-03-12 16:09:36 -0300

    first example of an pubSub

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.notificacao_pubsub = (event, context) => {
+  const pubsubMessage = Buffer.from(event.data, 'base64').toString();
+  if (pubsubMessage === 'ALARME') {
+    console.log('Alarme Ativado!');
+  } else {
+    console.log('Alarme Desativado.');
+  }
+}
